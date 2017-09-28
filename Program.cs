db1a81efb5e7068993edcd2d2656b3e31baaecba
fuyun <scfido@gmail.com>
2017-09-28 14:06:04 +0800

    完成Polly简单示例

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Polly;
+
+namespace pollyDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Polly Demo");
+            MixedPolicy();
+        }
+
+        static int Compute()
+        {
+            var zero = 0;
+            return 1 / zero;
+        }
+
+        private static void Retry()
+        {
+            try
+            {
+                var retryTwoTimesPolicy = Policy
+                        .Handle<DivideByZeroException>()
+                        .Retry(3, (ex, count) =>
+                        {
+                            Console.WriteLine("执行失败! 重试次数 {0}", count);
+                            Console.WriteLine("异常来自 {0}", ex.GetType().Name);
+                        });
+
+                retryTwoTimesPolicy.Execute(Compute);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine($"Excuted Failed,Message: ({e.Message})");
+
+            }
+        }
+
+        private static void Fallback()
+        {
+            var fallBackPolicy = Policy<int>
+                    .Handle<Exception>()
+                    .Fallback(0);
+
+            var fallBack = fallBackPolicy.Execute(Compute);
+            Console.WriteLine($"除零返回：{fallBack}");
+        }
+
+        private static void MixedPolicy()
+        {
+            var fallBackPolicy = Policy<int>
+                    .Handle<Exception>()
+                    .Fallback(0);
+
+            var politicaWaitAndRetry =Policy<int>
+                    .Handle<Exception>()
+                    .Retry(3, (ex, count) =>
+                    {
+                        Console.WriteLine("执行失败! 重试次数 {0}", count);
+                        Console.WriteLine("异常来自 {0}", ex.GetType().Name);
+                    });
+
+            var mixedPolicy = Policy.Wrap(fallBackPolicy, politicaWaitAndRetry);
+            var mixedResult = mixedPolicy.Execute(Compute);
+            Console.WriteLine($"执行结果: {mixedResult}");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+
+![](Polly-Logo@2x.png)
+
+我们来介绍一款强大的库Polly，Polly是一种.NET弹性和瞬态故障处理库，允许我们以非常顺畅和线程安全的方式来执诸如行重试，断路，超时，故障恢复等策略。
+
+# 项目示例
+运行示例
+```sh
+git clone https://github.com/scfido/pollyDemo.git
+cd pollyDemo
+dotnet run
+```
+
+# 参考资料
+* [已被.NET基金会认可的弹性和瞬态故障处理库Polly介绍](
+http://www.cnblogs.com/CreateMyself/p/7589397.html)
+* [Polly @GitHub](https://github.com/App-vNext/Polly)
